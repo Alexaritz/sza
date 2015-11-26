1dd35ce7dee711b9e854cc504017aa63c5233f4d
Alex Aranburu <aharanburu@gmail.com>
2015-11-26 23:14:58 +0100

    botoia inda(txuria), jartzea falta. Signup-login inda

---
+++ erabiltzailea.php
@@ -0,0 +1,27 @@
+<?php
+$BL_FILE='erabiltzaileak.xml';		// Iruzkinak gordeko diren fitxategia.
+
+function gorde_erabiltzailea($username, $name, $ $surnames, $email, $password)
+{
+	global $BL_FILE;	// Funtzio baten barrutik aldagai global erabiltzeko 'global' erabili behar da.
+	
+	if(!file_exists($BL_FILE))	// Iruzkinak gordetzeko XML fitxategia ez bada existitzen, sortu iruzkinik gabeko XML fitxategia.
+		$bl=new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE erabiltzaileak SYSTEM "erabiltzaileak.dtd"><erabiltzaileak azkenid="b0"></erabiltzaileak>');
+	else	// Bestela, kargatu XML fitxategia.
+		$bl=simplexml_load_file($BL_FILE);
+	if(!$bl)
+		return false;
+	
+	$id = "b" . ((int) substr($bl['azkenid'], 1) + 1);	// Kalkulatu erabiltzaile berriaren identifikadorea.
+	$berria=$bl->addChild('erabiltzailea');	// Sortu 'erabiltzailea' etiketa.
+	$berria['id']=$id;
+	$berria->addChild('username',$username);
+	$berria->addChild('name',$name);
+	$berria->addChild('surnames',$surnames);
+	$berria->addChild('email',$email);
+	$berria->addChild('password',$password);
+
+	$bl['azkenid']=$id;	// Eguneratu erroko 'azkenid' atributua.
+	return $bl->asXML($BL_FILE);	// Gorde aldaketak fitxategian.
+}
+?>
---
+++ signUp.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$servidor = "mysql.hostinger.es";//localhost mysql.hostinger.es
+$usuario = "u266570359_sza";//root u266570359_alex
+$password = "alexadri";// alexadri
+$sdb = "u266570359_sza";
+//$mysqli =new mysqli ($servidor,$usuario,$password, $sdb);
+$mysqli =new mysqli ("localhost","root","", $sdb);
+if ($mysqli->connect_error) {
+    printf("Connection failed: " . $mysqli->connect_error);
+} 
+//if ($_SERVER["REQUEST_METHOD"] == "post") {
+$Username= $_POST['username'];
+$Name= $_POST['izena'];
+$Surnames= $_POST['surnames'];
+$Email= $_POST['email'];
+$Pass= $_POST['pass'];
+//}
+$txertatu="INSERT INTO Erabiltzaile(Username, Name, Surnames, Email, Password) VALUES ('$Username','$Name','$Surnames','$Email', '$Pass')"; 
+if (!$mysqli -> query($txertatu)){
+	die("<p>Errorea gertatu da: ".$mysqli -> error ."</p>");	
+}else{
+	$_Session['email']=$Email;	
+	echo 'Erabiltzailea zuzen sartu da';
+	header("Location: serie.html");
+	die();
+}	
+?>
